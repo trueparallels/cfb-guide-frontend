f6e4f2689e9c5f02112804ebc4ee90be106fd29f
Kyle Peacock <kyle.peacock@urbanelectric.com>
2019-08-26 08:23:26 -0400

    clean up metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `CFB TV Guide`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Comprehensive FBS football schedules with TV times and channels`,
+    author: `@trueparallels`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
