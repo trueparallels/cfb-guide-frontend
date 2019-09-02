36db73a2c451e11e5be997a52007d17b65be5775
Kyle Peacock <kyle.peacock@urbanelectric.com>
2019-09-02 08:23:13 -0400

    show more finals on mobile

--- src/components/TeamImage.js
+++ src/components/TeamImage.js
@@ -5,7 +5,7 @@ const TeamImage = (props) => {
 
   if (isSmall) {
     return (
-      <img className="w-10 h-10" src={`https://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/${team.id}.png&h=80`} alt={team.location} />
+      <img className="w-6 h-6 sm:w-10 sm:h-10" src={`https://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/${team.id}.png&h=80`} alt={team.location} />
     )
   }
 
